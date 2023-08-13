1920e3f7354ec3beefd646cf0354263151c720ad
Ermiyas <ermiyas.dagnachew@gmail.com>
2023-08-13 17:36:18 +0300

    TRied IMage opt

---
+++ app/components/Intro.tsx
@@ -0,0 +1,84 @@
+"use client";
+import { useState } from "react";
+import useAOS from "../functions/useAOS";
+import Link from "next/link";
+interface Props {
+  img1?: string;
+  img2?: string;
+  title?: string;
+  description?: string[];
+  button?: string;
+  lr?: boolean;
+  link?: string;
+  subtitle?: string;
+}
+
+const Intro = (props: Props) => {
+  const [width, setWidth] = useState(true);
+  window.addEventListener("resize", () => {
+    if (window.innerWidth < 500) {
+      setWidth(false);
+    } else {
+      setWidth(true);
+    }
+  });
+  const style = {
+    transform: "translateX(50px)",
+  };
+  useAOS();
+  return (
+    <section id="about" className="home-intro">
+      {props.lr ? (
+        <div className="intro-container">
+          <div className="intro-text">
+            <h2 className="intro-heading">{props.title}</h2>
+            <h2 className="intro-subheading">{props.subtitle}</h2>
+            {props.description &&
+              props.description.map((description, index) => (
+                <p className="intro-description" key={index}>
+                  {description}
+                </p>
+              ))}
+            {props.button && (
+              <Link
+                className="offset"
+                href={props.link ? props.link : "/tours-by-experience"}
+              >
+                {props.button}
+              </Link>
+            )}
+          </div>
+          <div className="intro-img">
+            <img src={props.img1} alt="Intro image" />
+            <img src={props.img2} alt="Intro image" />
+          </div>
+        </div>
+      ) : (
+        <div className="intro-container">
+          <div className="intro-img" style={width ? style : {}}>
+            <img src={props.img1} alt="Intro image" />
+            <img src={props.img2} alt="Intro image" />
+          </div>
+          <div className="intro-text">
+            <h2 className="intro-heading">{props.title}</h2>
+            <h2 className="intro-subheading">{props.subtitle}</h2>
+            {props.description &&
+              props.description.map((description, index) => (
+                <p className="intro-description" key={index}>
+                  {description}
+                </p>
+              ))}
+
+            {props.button && (
+              <a className="offset" href={props.link}>
+                {props.button}
+              </a>
+            )}
+          </div>
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default Intro;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
 import Images from "./Data/Images";
 import Hero from "./components/Hero";
+import Intro from "./components/Intro";
 
 export default function Home() {
   return (
     <>
       <Hero background={Images.lalibela5} index title="Home" link="#about" />
+      <Intro
+        img1={Images.intro1}
+        img2={Images.intro2}
+        title="Welcome to Sawla Tours"
+        subtitle="Where Extraordinary Journeys Unfold"
+        description={[
+          "We're not just a travel company; we're your key to unlocking unforgettable adventures in the heart of Ethiopia, the mesmerizing jewel of Africa.",
+          "Our mission? To unveil Ethiopia's enchanting magic, tailored uniquely to you. With us, travel is not a one-size-fits-all affair but a deeply personalized journey, sculpted around your desires, budget, and adventure spirit.",
+        ]}
+        lr
+        button="About Us"
+        link="/about-us"
+      />
     </>
   );
 }
